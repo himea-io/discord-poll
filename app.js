a4f1da9f34a319bda38cd4389e82c3fdc7d01093
Cédric <triozer1@gmail.com>
2018-10-28 15:02:41 +0100

    changing routes prefixe

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ const client = new Discord.Client()
 // trying to login to discord if fails kill the app
 client.login(process.env.TOKEN).then(() => {
   // all routes prefixed by auth
-  app.use('/auth', require('./src/routes/auth').router)
+  app.use('/poll/auth', require('./src/routes/auth').router)
   // all routes prefixed by panel
-  app.use('/panel', require('./src/routes/panel').router)
+  app.use('/poll/panel', require('./src/routes/panel').router)
   // all routes prefixed by api/v1
-  app.use('/api/v1', require('./src/routes/v1').router)
+  app.use('/poll/api/v1', require('./src/routes/v1').router)
 }).catch((error) => {
   console.log(error)
   process.exit(1)
