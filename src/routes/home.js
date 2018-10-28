62f7fb9a82058af84635f96487a61dfa3d51a758
Cédric <triozer1@gmail.com>
2018-10-28 17:04:06 +0100

    redirections are too fast x)

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 // set routes
-
 router.get('/', (req, res) => res.render('home', {auth: req.isAuthenticated(), user: req.user}))
 
 module.exports.router = router
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,5 @@ const client = require('../../app').client
 
 module.exports.auth = (req, res, next) => { 
     if (req.isAuthenticated()) return next()
+    res.redirect('/poll')
 }
