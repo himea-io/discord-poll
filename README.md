ac08f6564cea1efe43bc645f8b218e0a96b48062
Cédric <triozer1@gmail.com>
2018-10-28 11:21:40 +0100

    boum

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # discord-poll
 A discord-based poll bot with web panel.
+
+## Setup
+
+1. Clone the repository and access it `git clone https://github.com/himea-io/discord-poll.git && cd discord-rest`.
+2. Set your discord bot token and the server port (default 3002) in the `.env` file.
+3. Type `npm install`.
+4. Type `npm start`.
+5. Enjoy your stuff!
+
+## Demo
+
+See this in action at : https://himea.io/poll/
---
+++ app.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js')
+const express = require('express')
+const logger = require('morgan')
+const cors  = require('cors')
+require('dotenv').config()
+
+// setup express -
+const app = express()
+app.use(cors())
+app.use(logger('dev'))
+
+// setup the discord bot -
+const client = new Discord.Client()
+// trying to login to discord if fails kill the app
+client.login(process.env.TOKEN).then(() => {
+  // all routes prefixed by panel
+  app.use('/panel', require('./src/routes/panel').router)
+  // all routes prefixed by api/v1
+  app.use('/api/v1', require('./src/routes/v1').router)
+}).catch((error) => {
+  console.log(error)
+  process.exit(1)
+})
+// send a message on login
+client.on('ready', () => console.log(`Discord bot started\n`))
+
+module.exports.app = app
+module.exports.client = client
